c67fc95672a1a5b4b24633be4a119c7a776abbe2
Santiago Torres <storresb@pop-os.localdomain>
2022-01-06 22:13:40 +0100

    changed arguments in emergency scripts

--- EmergencyCall/noemergency.sh
+++ EmergencyCall/noemergency.sh
@@ -2,6 +2,6 @@
 # $1: MESSAGE TO EMIT
 # $2: IP ADDRESS OF SERVER
 # $3: SERVICE PORT
-echo -n 'Y' | nc -w 1 $2 $3
+echo -n 'Y' | nc -w 1 $1 $2
 
 # example : bash webput.sh '1' 127.0.0.0 8080
--- EmergencyCall/thereisemergency.sh
+++ EmergencyCall/thereisemergency.sh
@@ -2,6 +2,6 @@
 # $1: MESSAGE TO EMIT
 # $2: IP ADDRESS OF SERVER
 # $3: SERVICE PORT
-echo -n 'N' | nc -w 1 $2 $3
+echo -n 'N' | nc -w 1 $1 $2
 
 # example : bash webput.sh '0' 127.0.0.0 8080
